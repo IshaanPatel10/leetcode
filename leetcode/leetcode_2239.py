08dc483810ba553bcb185b33fc2909ff8a3b8e20
IshaanPatel10 <ishaanpatel7007@gmail.com>
2026-03-30 23:14:33 -0500

    leetcode 2239

--- leetcode/leetcode_2239.py
+++ leetcode/leetcode_2239.py
@@ -1,1 +1,15 @@
-print("hello")
+class Solution:
+    def findClosestNumber(nums):
+        
+        closest = float('inf')
+
+        for i in nums:
+            if abs(i) < abs(closest):
+                closest = i
+        
+        if abs(closest) in nums and closest < 0:
+            return abs(closest)
+        
+        return closest
+
+    findClosestNumber([-4,-2,1,4,8])
